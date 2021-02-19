0acb1c15fcf3f820efc4d87ffd92e4157ddbe193
manideepu <manideepu@Dev-CS-Mac-2.local>
2021-02-19 11:59:17 +0530

    modified

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -3,13 +3,20 @@ package com.coveros.demo.helloworld;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static java.lang.System.*;
+
 public class HelloWorld {
 
   public static void main(final String[] args) {
     final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
     final LocalDateTime now = LocalDateTime.now();
 
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
+    out.println("Hello, World! The current time is " + dtf.format(now));
   }
+
+  public Demo() {
+    throw new UnsupportedOperationException();
+  }
+
 
 }
